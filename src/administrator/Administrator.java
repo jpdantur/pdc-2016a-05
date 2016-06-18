2bb5ca969e0325217247bf0a5f608f373c94b70b
Matias Rivas <rivasleandromatias@gmail.com>
2016-06-17 21:00:19 -0300

    se borran comentarios

--- src/administrator/Administrator.java
+++ src/administrator/Administrator.java
@@ -25,42 +25,31 @@ public class Administrator implements Runnable{
     public void run() {
         logger.info("Administrator is running.");
         try {
-            // Create a selector to multiplex listening sockets and connections
             Selector selector = Selector.open();
-            // Create listening socket channel for each port and register selector
             ServerSocketChannel listnChannel = ServerSocketChannel.open();
             listnChannel.socket().bind(new InetSocketAddress(config.getConfiguration().getAdmin().get(0).getPort()));
-            listnChannel.configureBlocking(false); // must be nonblocking to
-            // Register selector with channel. The returned key is ignored
+            listnChannel.configureBlocking(false);
             listnChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-            // Create a handler that will implement the protocol
             TCPProtocol protocol = new AdministratorProtocol(config.getConfiguration().getBufferSize(),
                     config.getConfiguration().getAdmin().get(0));
 
-            while (true) { // Run forever, processing available I/O operations
-                // Wait for some channel to be ready (or timeout)
-                if (selector.select(TIMEOUT) == 0) { // returns # of ready chans
+            while (true) {
+                if (selector.select(TIMEOUT) == 0) {
                     continue;
                 }
-                // Get iterator on set of keys with I/O to process
                 Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
                 while (keyIter.hasNext()) {
-                    SelectionKey key = keyIter.next(); // Key is bit mask
-                    // Server socket channel has pending connection requests?
+                    SelectionKey key = keyIter.next();
                     if (key.isAcceptable()) {
                         protocol.handleAccept(key, this);
                     }
-                    // Client socket channel has pending data?
                     if (key.isReadable()) {
                         protocol.handleRead(key, this);
                     }
-                    // Client socket channel is available for writing and
-                    // key is valid (i.e., channel not closed)?
                     if (key.isValid() && key.isWritable()) {
                         protocol.handleWrite(key, this);
                     }
-                    keyIter.remove(); // remove from set of selected keys
+                    keyIter.remove();
                 }
             }
         } catch (IOException ioe) {
